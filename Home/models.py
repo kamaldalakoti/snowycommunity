d5f1707cc9c05d9c2e76b64b44d6ea02610d5e18
kamaldalakoti <kamaldalakoti900@gmail.com>
2020-07-27 20:29:55 +0530

    demo2

--- Home/models.py
+++ Home/models.py
@@ -5,6 +5,7 @@ class signup(models.Model):
     Name = models.CharField(max_length=20)
     Phone = models.CharField(max_length=30)
     PubgID = models.TextField(max_length=20  )
+    whats = models.TextField(max_length=20  )
     City = models.TextField(max_length=110  )
     District = models.TextField(max_length=110  )
     State = models.TextField(max_length=110  )
