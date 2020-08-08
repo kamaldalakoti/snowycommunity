097e8324b99be9140ddecb8debbd47e22191adbd
kamaldalakoti <kamaldalakoti900@gmail.com>
2020-08-08 20:09:02 +0530

    nw one2

--- Home/models.py
+++ Home/models.py
@@ -7,9 +7,9 @@ class signup(models.Model):
     # gen = models.ChoicesField()
     # PubgID = models.TextField(max_length=20  )
     # whats = models.TextField(max_length=20  )
-    City = models.TextField(max_length=110  )
-    District = models.TextField(max_length=110  )
-    State = models.TextField(max_length=110  )
+    City = models.TextField(max_length=20  )
+    District = models.TextField(max_length=20  )
+    State = models.TextField(max_length=20  )
     # Pic = models.FileField(upload_to="static", storage=None)
     def __str__(self):
         return self.Name    
