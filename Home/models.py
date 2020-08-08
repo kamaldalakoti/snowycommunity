06c78b295e3423f66b89921dd020f2fd3108603b
kamaldalakoti <kamaldalakoti900@gmail.com>
2020-08-08 16:25:39 +0530

    nw one

--- Home/models.py
+++ Home/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class signup(models.Model):
     Name = models.CharField(max_length=20)
     Phone = models.CharField(max_length=30)
-    PubgID = models.TextField(max_length=20  )
-    whats = models.TextField(max_length=20  )
+    # gen = models.ChoicesField()
+    # PubgID = models.TextField(max_length=20  )
+    # whats = models.TextField(max_length=20  )
     City = models.TextField(max_length=110  )
     District = models.TextField(max_length=110  )
     State = models.TextField(max_length=110  )
-    Pic = models.FileField(upload_to="static", storage=None)
+    # Pic = models.FileField(upload_to="static", storage=None)
     def __str__(self):
         return self.Name    
